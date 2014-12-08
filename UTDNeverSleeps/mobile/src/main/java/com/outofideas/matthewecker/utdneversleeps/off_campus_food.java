3281c93b87115759a6e52880599315049740f2fe
Matt <gutlessVADER@gmail.com>
2014-12-08 15:17:32 -0600

    formatting

--- UTDNeverSleeps/mobile/src/main/java/com/outofideas/matthewecker/utdneversleeps/off_campus_food.java
+++ UTDNeverSleeps/mobile/src/main/java/com/outofideas/matthewecker/utdneversleeps/off_campus_food.java
@@ -6,31 +6,35 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class off_campus_food extends Activity {
+public class off_campus_food extends Activity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_off_campus_food);
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_off_campus_food, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
